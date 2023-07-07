2aea1df1bdc8dd16d3a4b296dc7f80bfdb35190f
Nanang Arifudin <nanangarif404@gmail.com>
2023-07-07 18:54:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gigih3/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gigih3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gigih3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        with(binding){
+
+        }
+
+    }
+}
